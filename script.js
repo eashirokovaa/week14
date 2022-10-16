d1621d7fa0905413f6a0fdd9f5003c1e19248e94
eashirokovaa <eashirokovaa@gmail.com>
2022-10-16 22:06:50 +0300

    Update

--- script.js
+++ script.js
@@ -13,22 +13,22 @@ function calcSum() {
   document.getElementById("result").value = sum;
 }
 function calcSubtract() {
-  let digit3 = Number(document.getElementById("digit3").value);
-  let digit4 = Number(document.getElementById("digit4").value);
-  sum1 = digit3 - digit4;
-  document.getElementById("result1").value = sum1;
+  let digit1 = Number(document.getElementById("digit1").value);
+  let digit2 = Number(document.getElementById("digit2").value);
+  sum = digit1 - digit2;
+  document.getElementById("result").value = sum;
 }
 function calcMult() {
-  let digit5 = Number(document.getElementById("digit5").value);
-  let digit6 = Number(document.getElementById("digit6").value);
-  sum2 = digit5 * digit6;
-  document.getElementById("result2").value = sum2;
+  let digit1 = Number(document.getElementById("digit1").value);
+  let digit2 = Number(document.getElementById("digit2").value);
+  sum = digit1 * digit2;
+  document.getElementById("result").value = sum;
 }
 function calcDiv() {
-  let digit7 = Number(document.getElementById("digit7").value);
-  let digit8 = Number(document.getElementById("digit8").value);
-  sum3 = digit7 / digit8;
-  document.getElementById("result3").value = sum3;
+  let digit1 = Number(document.getElementById("digit1").value);
+  let digit2 = Number(document.getElementById("digit2").value);
+  sum = digit1 / digit2;
+  document.getElementById("result").value = sum;
 }
 function forwardImg() {
   let changedImg = document.getElementById("catImage");
